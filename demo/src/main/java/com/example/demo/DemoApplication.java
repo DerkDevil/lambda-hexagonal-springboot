88f0a2b128a72af9d0d8a933d34917621b8b6c1a
Dereck Sebastian Muñoz Castiblanco <munozderecksebastian@gmail.com>
2025-06-17 13:37:24 -0500

    fix context bean

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.example.demo.application.usecases.ConsultarUsuarioUseCase;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Map;
 import java.util.function.Function;
 
 @SpringBootApplication
@@ -13,10 +18,26 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
-	public Function<String, String> uppercase() {
-		return value -> value.toUpperCase();
+	@Bean("consultarUsuarioFunction")
+	public Function<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>
+	consultarUsuarioFunction(ConsultarUsuarioUseCase useCase,
+							 ObjectMapper objectMapper) {
+		return request -> {
+			try {
+				String id = objectMapper.readTree(request.getBody())
+						.get("id").asText();
+				var u = useCase.consultarPorId(id);
+				return new APIGatewayProxyResponseEvent()
+						.withStatusCode(200)
+						.withHeaders(Map.of("Content-Type", "application/json"))
+						.withBody(objectMapper.writeValueAsString(u));
+			}
+			catch (Exception e) {
+				return new APIGatewayProxyResponseEvent()
+						.withStatusCode(500)
+						.withBody("{\"error\":\""+e.getMessage()+"\"}");
+			}
+		};
 	}
 
 }
-
